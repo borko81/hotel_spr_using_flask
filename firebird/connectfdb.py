cab7a58625c12fd67bf5f89bf81b0d9a829b3ec8
Borko <bstoilov81@gmail.com>
2021-05-02 21:38:44 +0300

    Add search button in some templates

--- firebird/connectfdb.py
+++ firebird/connectfdb.py
@@ -1,6 +1,6 @@
 import fdb
 
-database = {"host": "192.168.168.12", "database": "hotel_spr", "user": "username", "password": "password"}
+database = {"host": "ip", "database": "hotel_spr", "user": "user", "password": "password"}
 
 
 def con_to_firebird(query, *args):
